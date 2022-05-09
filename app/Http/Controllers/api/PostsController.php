664df28aee67e0186aa7d51c2a4fe4d8034d211f
gustavobraga1001 <gubraro0@gmail.com>
2022-05-09 08:35:42 -0300

    first commit

---
+++ app/Http/Controllers/api/PostsController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Posts;
+
+
+
+class PostsController extends Controller
+
+{
+
+    public function index()
+    {
+        $posts = Posts::all('id','title','description');
+        return $posts;
+    }
+
+
+    public function store(Request $request)
+    {
+        Posts::create($request->all());
+        return $response = json_encode([
+            "error" => false,
+            "mensage" => "Post publicado com successo"
+        ]);
+    }
+
+
+    public function show($id)
+    {
+        return Posts::findOrFail($id, ['title','description']);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $post = Posts::findOrFail($id);
+        $post->update($request->all());
+        return $response = json_encode([
+            "error" => false,
+            "mensage" => "Post atualizado com sucesso!"
+        ]);
+    }
+
+
+    public function destroy($id)
+    {
+        $post = Posts::findOrFail($id);
+        $post->delete();
+        return $response = json_encode([
+            "error" => false,
+            "mensage" => "Post deletado com sucesso!"
+        ]);
+    }
+}
